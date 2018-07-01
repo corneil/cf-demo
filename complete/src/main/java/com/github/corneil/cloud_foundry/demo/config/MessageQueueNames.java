1af4ef28dac52ddeb2d4348f194c4bf90f8abacc
Corneil du Plessis <corneil.duplessis@gmail.com>
2018-07-01 19:27:52 +0200

    polishing

--- complete/src/main/java/com/github/corneil/cloud_foundry/demo/config/MessageQueueNames.java
+++ complete/src/main/java/com/github/corneil/cloud_foundry/demo/config/MessageQueueNames.java
@@ -1,7 +1,10 @@
 package com.github.corneil.cloud_foundry.demo.config;
 
 public class MessageQueueNames {
-    public static String QUEUE_NAME = "cf-demo-queue";
-    public static String TOPIC_EXCHANGE_NAME = "cf-demo-topic-exchange";
-    public static String ROUTING_KEY = "cf-demo-key";
+    private MessageQueueNames() {
+    }
+
+    public static final String QUEUE_NAME = "cf-demo-queue";
+    public static final String TOPIC_EXCHANGE_NAME = "cf-demo-topic-exchange";
+    public static final String ROUTING_KEY = "cf-demo-key";
 }
