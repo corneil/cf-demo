3166529bbd849bf80d1f1a1c814d7c93755764e4
Corneil du Plessis <corneil.duplessis@gmail.com>
2018-07-11 13:20:00 +0200

    Polishing

---
+++ complete/src/main/java/com/github/corneil/cloud_foundry/demo/model/EventService.java
@@ -0,0 +1,19 @@
+package com.github.corneil.cloud_foundry.demo.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.Date;
+import java.util.stream.Stream;
+
+public interface EventService {
+
+    Event createEvent(String eventSource) throws JsonProcessingException;
+
+    Stream<Event> listAll();
+
+    Stream<Event> listAll(Date startDate, Date endDate);
+
+    Stream<Event> listByEventSource(String eventSource);
+
+    Stream<Event> listByEventSource(String eventSource, Date startDate, Date endDate);
+}
