e3bcd98bd5d9210e366fc2c35179d96f2d1e50ac
Corneil du Plessis <corneil.duplessis@gmail.com>
2018-07-12 10:45:36 +0200

    Polishing

---
+++ loader/src/main/java/com/github/corneil/cloud_foundry/loader/LoaderRetryListener.java
@@ -0,0 +1,27 @@
+package com.github.corneil.cloud_foundry.loader;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.RetryListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LoaderRetryListener implements RetryListener {
+    @Override
+    public <T, E extends Throwable> boolean open(RetryContext context, RetryCallback<T, E> callback) {
+        log.info("retry:open:{}", context);
+        return true;
+    }
+
+    @Override
+    public <T, E extends Throwable> void close(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+        log.info("retry:close:count={},exhausted={}", context.getRetryCount(), context.isExhaustedOnly());
+    }
+
+    @Override
+    public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+        log.error("retry:error:{}:{}", context, throwable);
+    }
+}
